7c2023a1c0a15e9b95875b26968e7eb85540a1fd
Choieeh <chyh0182@naver.com>
2020-11-21 14:53:29 +0000

    data별로 출력하기

--- main.js
+++ main.js
@@ -18,7 +18,8 @@ var app = http.createServer(function(request,response){
       return;
     }
     response.writeHead(200);
-	var template = `
+	fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+		var template = `
 	<!doctype html>
 	<html>
 	<head>
@@ -33,12 +34,15 @@ var app = http.createServer(function(request,response){
     	<li><a href="/?id=JavaScript">JavaScript</a></li>
   	</ul>
   	<h2>${title}</h2>
-  	<p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
+  	<p>${description}
   	</p>
 	</body>
 	</html>
-`
-    response.end(template);
+`;
+		response.end(template);
+	})
+	
+    
 	//response.end('egoing : '+url); 하면 'egoing : /index.html'로 들어가짐.
  
 });
