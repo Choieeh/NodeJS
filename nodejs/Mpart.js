088d589e5d6053f2b2f5045a8e1cc17a3b08a053
Choieeh <chyh0182@naver.com>
2020-12-07 13:35:45 +0000

    use Module

---
+++ nodejs/Mpart.js
@@ -0,0 +1,9 @@
+var M = {
+	v:'v',
+	f:function(){
+		console.log(this.v);
+	}
+}
+
+module.exports = M;
+//밖에서 사용할수 있도록 M을 export 함.
---
+++ nodejs/muse.js
@@ -0,0 +1,12 @@
+/*var M = {
+	v:'v',
+	f:function(){
+		console.log(this.v);
+	}
+}
+*/
+
+var part = require('./Mpart.js');
+//export한걸 받아올수 있는 require
+
+part.f();
