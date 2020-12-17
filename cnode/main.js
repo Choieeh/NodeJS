2075fd914776ef007d14cb75920bfd26bde0833f
Choieeh <chyh0182@naver.com>
2020-12-17 13:37:33 +0000

    list 목록 받아오기

--- cnode/main.js
+++ cnode/main.js
@@ -3,21 +3,27 @@ var fs = require('fs');
 //fs 모듈로 파일 입출력
 var url = require('url');
 
-var app = http.createServer(function(request,response){
-	//요청이 오면 실행되는 콜백 함수
-	var _url = request.url;
-	var queryData = url.parse(_url, true).query;
-	//url.parse는 url의 정보를 가져옴. true는 query속성을 객체 형식으로 가져오고 false는 query 속성을 문자열 형식으로 가져옴.
-	var pathname = url.parse(_url, true).pathname;
-	//path는 querystring 포함 pathname은 불포함
-	
-	if(pathname === '/'){
-		if(queryData.id === undefined){
-			
-			fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
-				var title = 'Welcome';
-				var description = 'Hello, Node.js';
-				var template = `
+var app = http.createServer(function (request, response) {
+    //요청이 오면 실행되는 콜백 함수
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    //url.parse는 url의 정보를 가져옴. true는 query속성을 객체 형식으로 가져오고 false는 query 속성을 문자열 형식으로 가져옴.
+    var pathname = url.parse(_url, true).pathname;
+    //path는 querystring 포함 pathname은 불포함
+
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            fs.readdir('./data', function (error, filelist) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+                var template = `
 					<!doctype html>
 					<html>
 					<head>
@@ -26,61 +32,59 @@ var app = http.createServer(function(request,response){
 					</head>
 					<body>
 					  <h1><a href="/">WEB</a></h1>
-					  <ul>
-						<li><a href="/?id=HTML">HTML</a></li>
-						<li><a href="/?id=CSS">CSS</a></li>
-						<li><a href="/?id=JavaScript">JavaScript</a></li>
-					  </ul>
+					  ${list}
 					  <h2>${title}</h2>
 					  <p>${description}</p>
 					</body>
 					</html>
 					`;
-				response.writeHead(200);
-			//http 프로토콜 header지정
-				response.end(template);
-					});
-		} else{
-			fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
-				
-		var title = queryData.id;
-		var template = `
-			<!doctype html>
-			<html>
-			<head>
-			  <title>WEB1 - ${title}</title>
-			  <meta charset="utf-8">
-			</head>
-			<body>
-			  <h1><a href="/">WEB</a></h1>
-			  <ul>
-				<li><a href="/?id=HTML">HTML</a></li>
-				<li><a href="/?id=CSS">CSS</a></li>
-				<li><a href="/?id=JavaScript">JavaScript</a></li>
-			  </ul>
-			  <h2>${title}</h2>
-			  <p>${description}</p>
-			</body>
-			</html>
-			`;
-		response.writeHead(200);
-	//http 프로토콜 header지정
-		response.end(template);
-			});
-		}
-		
-	} else{
-		response.writeHead(404);
-		//web서버와 web브라우저가 오류를 확인하기 위한 숫자
-		//즉 아무것도 없으면 서버가 404라는 숫자를 넘기게됨.
-		response.end('Not found');
-	}
-	
-	
-	
-//	response.end(fs.readFileSync(queryData.id));
-	//response.end는 응답 데이터 전송 즉 readFileSync함수를 실행
-	/*readFileSync는 filename의 파일을 [options]의 방식으로 읽은 후 callback으로 전달된 함수를 호출한다.
+                response.writeHead(200);
+                //http 프로토콜 header지정
+                response.end(template);
+            });
+        } else {
+            fs.readdir('./data', function (error, filelist) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i = i + 1;
+                }
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                    var title = queryData.id;
+                    var template = `
+						<!doctype html>
+						<html>
+						<head>
+						  <title>WEB1 - ${title}</title>
+						  <meta charset="utf-8">
+						</head>
+						<body>
+						  <h1><a href="/">WEB</a></h1>
+						  ${list}
+						  <h2>${title}</h2>
+						  <p>${description}</p>
+						</body>
+						</html>
+						`;
+                    response.writeHead(200);
+                    //http 프로토콜 header지정
+                    response.end(template);
+                });
+            });
+        }
+    } else {
+        response.writeHead(404);
+        //web서버와 web브라우저가 오류를 확인하기 위한 숫자
+        //즉 아무것도 없으면 서버가 404라는 숫자를 넘기게됨.
+        response.end('Not found');
+    }
+
+    //	response.end(fs.readFileSync(queryData.id));
+    //response.end는 응답 데이터 전송 즉 readFileSync함수를 실행
+    /*readFileSync는 filename의 파일을 [options]의 방식으로 읽은 후 callback으로 전달된 함수를 호출한다.
 	Sync라는 이름이 붙어있는 메소드는 동기방식을 사용한다.
 	동기적 방식은 파일을 읽으면서 다른 작업을 동시에 할 수 없음.
 	비동기 형식은 항상 마지막 인수가 수행 완료 시 호출할 콜백 함수로 작성되어야 함
